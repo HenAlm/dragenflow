305fa4e096586f084cbfac3d4a0471fcc97b40fe
HenAlm <henrikki.almusa@helsinki.fi>
2023-09-25 10:36:58 +0300

    use fastq inputs for tumor normal pair to run it with one command

--- src/dragen_pipeline.py
+++ src/dragen_pipeline.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import List, Optional
 
 from .dragen_commands import (
@@ -34,6 +35,7 @@ from .utility.flow import Flow
 class ConstructDragenPipeline(Flow):
     def __init__(self):
         self.normals = {}
+        self.commands = {}
         self.profile = None
 
     def add_cnv(self, excel: dict, cmd: dict) -> bool:
@@ -84,6 +86,34 @@ class ConstructDragenPipeline(Flow):
                 new_list.write(add_normal)
         cmd["cnv-normals-list"] = new_panel
 
+    def get_normal_params(self, normal_key:str) -> dict:
+        normal = self.normals[normal_key]
+        replay_f = f"{normal}-replay.json"
+        params = {"fastq-file1" : None, "fastq-file2" : None,
+                  "RGID": None, "RGSM": None
+        }
+        if normal_key in self.commands:
+            for i in self.commands[normal_key]:
+                if i in params:
+                    params[i] = self.commands[normal_key][i]
+        elif os.path.isfile(replay_f):
+            normal_replay = load_json(f"{normal}-replay.json")
+            for i in normal_replay["dragen_config"]:
+                if i["name"] in params:
+                    params[i["name"]] = i["value"]
+        else:
+            raise ValueError(f"Unable to get normal fastq parameters.")
+        for i in ["fastq-file1","fastq-file2"]:
+            if not os.path.isabs(params[i]):
+                params[i] = os.path.normpath(os.path.join(os.path.dirname(normal),params[i]))
+        for i in params:
+            if params[i] == None:
+                raise ValueError(f"Missing option '{i}'")
+        return params
+
+    def save_command(self, key:str, command:dict) -> None:
+        self.commands[key] = command
+
     def constructor(self, excel: dict) -> Optional[List[str]]:
         self.profile = load_json(script_path("dragen_config.json"))["profile1"]
         # load pre and post scripts
@@ -120,6 +150,7 @@ class ConstructDragenPipeline(Flow):
                 f"{excel[SH_SM_PROJ]}/{excel[SH_SAMPLE]}"
             ] = f"../{excel[SH_SAMPLE]}/{cmd_d['output-file-prefix']}"
             cmd_d.update(add_options(excel[SH_OVERRIDE]))
+            self.save_command(f"{excel[SH_SM_PROJ]}/{excel[SH_SAMPLE]}",cmd_d)
             final_str = dragen_cli(cmd=cmd_d, excel=excel, scripts=scripts)
             return [final_str]
 
@@ -137,17 +168,15 @@ class ConstructDragenPipeline(Flow):
             return [final_str]
 
         elif excel[SHA_RTYPE] == "somatic_paired":
+            # if umi, then need to do two commands
+            # otherwise need both tumor and normal fastq, sample name and rgid
             arg_string = []
-            base_cmd = BaseDragenCommand(
-                excel, self.profile, f"{pipeline}_paired_variant_call"
-            )
-            cmd_d2 = CompositeCommands()
             normal_prefix = (f"{excel[SH_SM_PROJ]}/{excel[SH_NORMAL]}")
             if excel[SHA_NPATH]:
                 normal_prefix = normal_prefix + "/EXTERNAL"
                 self.normals[normal_prefix] = (f"{excel[SHA_NPATH]}/{excel[SH_NORMAL]}")
             if pipeline.startswith("umi"):
-                # step 1
+                # step 1 alignment
                 logging.info(f"{excel[SHA_RTYPE]}: preparing {pipeline} alignment template")
                 cmd_d1 = self.umi_pipeline(excel, "tumor_alignment", True)
                 cmd_d1.update(add_options(excel[SH_OVERRIDE],OPT_T_ALIGN))
@@ -155,31 +184,33 @@ class ConstructDragenPipeline(Flow):
                     cmd=cmd_d1, excel=excel, postf="alignment", scripts=scripts
                 )
                 arg_string.append(final_str1)
-            else:
-                logging.info(f"{excel[SHA_RTYPE]}: preparing tumor alignment template")
-                # step 1 tumor alignment
-                cmd_d1 = self.command_with_trim(excel, "tumor_alignment")
-                if self.add_cnv(excel, cmd_d1):
-                    cmd_d1["enable-map-align-output"] = "true"
-                    self.sample_pon(normal_prefix, excel["dry_run"], excel["fastq_dir"], cmd_d1)
-                cmd_d1.update(add_options(excel[SH_OVERRIDE],OPT_T_ALIGN))
-                final_str1 = dragen_cli(
-                    cmd=cmd_d1, excel=excel, postf="alignment", scripts=scripts
+                # step 2 paired variant call
+                base_cmd = BaseDragenCommand(
+                    excel, self.profile, f"{pipeline}_paired_variant_call"
                 )
-                arg_string.append(final_str1)
-            # step 2 paired variant call
-            logging.info(f"{excel[SHA_RTYPE]}: preparing paired variant call template")
-            # pv_cmd = PairedVariantCommands(f"{self.normals[normal_prefix]}.bam", cmd_d1)
-            pv_cmd = PairedVariantCommands(f"{self.normals[normal_prefix]}", cmd_d1, self.profile[f"{pipeline}_paired_variant_call"])
-            cmd_d2.add(base_cmd)
-            cmd_d2.add(pv_cmd)
-            cmd_d=cmd_d2.construct_commands()
-            cmd_d.update(self.check_liquid_tumor(excel))
-            cmd_d.update(add_options(excel[SH_OVERRIDE],OPT_T_ANALYSIS))
-            final_str2 = dragen_cli(
-                cmd=cmd_d, excel=excel, postf="analysis", scripts=scripts,
-            )
-            arg_string.append(final_str2)
+                cmd_d2 = CompositeCommands()
+                logging.info(f"{excel[SHA_RTYPE]}: preparing paired variant call template")
+                # pv_cmd = PairedVariantCommands(f"{self.normals[normal_prefix]}.bam", cmd_d1)
+                pv_cmd = PairedVariantCommands(f"{self.normals[normal_prefix]}", cmd_d1, self.profile[f"{pipeline}_paired_variant_call"])
+                cmd_d2.add(base_cmd)
+                cmd_d2.add(pv_cmd)
+                cmd_d=cmd_d2.construct_commands()
+                cmd_d.update(self.check_liquid_tumor(excel))
+                cmd_d.update(add_options(excel[SH_OVERRIDE],OPT_T_ANALYSIS))
+                final_str2 = dragen_cli(
+                    cmd=cmd_d, excel=excel, postf="analysis", scripts=scripts,
+                )
+                arg_string.append(final_str2)                
+            else:
+                logging.info(f"{excel[SHA_RTYPE]}: preparing tumor normal template")
+                # step 1 tumor alignment
+                cmd = self.command_with_trim(excel, "tumor_normal")
+                if self.add_cnv(excel, cmd):
+                    self.sample_pon(normal_prefix, excel["dry_run"], excel["fastq_dir"], cmd)
+                cmd.update(add_options(excel[SH_OVERRIDE],OPT_T_ALIGN))
+                cmd.update(self.get_normal_params(normal_prefix))
+                final_str = dragen_cli(cmd=cmd, excel=excel, scripts=scripts)
+                arg_string.append(final_str)
             return arg_string
 
         else:
